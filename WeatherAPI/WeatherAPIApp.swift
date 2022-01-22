b82d53dac2789faeab386de17430325a55ad96c6
Reza <rezafh19@gmail.com>
2022-01-22 16:00:58 +0700

    Initial Commit

---
+++ WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Reza Harris on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
